f07959f2f8e33789dff3970af6ecb21e397db3ea
KittyLee <katherine@codeforprogress.org>
2014-04-30 10:15:13 -0400

    Little game made in python

---
+++ Game2.py
@@ -0,0 +1,91 @@
+# Are You From Red Hook?
+
+
+print "Welcome to: Are You From Red Hook?"
+
+#Section 1. First question
+print "1. What borough is Red Hook in?"
+print "\n(a) Washington  (b) Queens  (c) Brooklyn (d) Bronx"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="c":
+	print "Damn Right!!"
+
+else:
+	print "Your Dead Wrong"	
+	print "The Answer: Red Hook is in Brooklyn."
+
+
+
+	#Section 2. Second question
+print "\n2. What movie director made a really bad movie about Red Hook?"
+print "\n(a) Michael Bay  (b) Spike Lee  (c) Quintin Tarrantino (d) Tim Burrton"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="b":
+	print "Damn Right!!"
+
+else:
+	print "Your Dead Wrong"	
+	print "The Answer: Spike Lee. The Movie was called Red Hook Summer "
+
+	#Section 3. Third question
+print "\n3. What famouse artist made a music video in Red Hook last year?"
+print "\n(a) Lorde  (b) 50 Cent  (c) Mick Jagger (d) Beyonce"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Damn Right!!"
+
+else:     print "Your Dead Wrong"     
+		  print "The Answer: Lorde. She filmed her song Team in Red Hook at our famouse abandon building called the Gray House."
+
+	#Section 4. Forth question
+print "\n4. What TV show is constantly in Red Hook?"
+print "\n(a) Vampire Diaries  (b) Silicon Valley (c) Brooklyn 99 (d) Law and Order"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="d":
+	print "Damn Right!!"
+
+else:
+	print "Your Dead Wrong"	
+	print "The Answer: Law and Order. They always come down to Red Hook when looking for killers at our piers."
+
+
+	#Section 5. Fith question
+print "\n5. What big market came to Red Hook almost 5 years ago?"
+print "\n(a) Fairway (b) Home Depot  (c) Ikea (d) Lowes"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="c":
+	print "Damn Right!!"
+
+else:
+	print "Your Dead Wrong"	
+	print "The Answer: Ikea. It took a long time to get Ikea because of all the backlash but almost 5 years ago it finally happened."
+
+
+	#Section 6. Sixth question
+print "\n6. What major hurricane flooded Red Hook in 2012?"
+print "\n(a) Hurricane Sandy (b) Hurricane Irene (c) Hurricane Isabel (d) Hurricane Issac "	
+
+answer = raw_input("Your answer: ")
+
+if answer=="a"
+	print "Damn Right!!"
+
+else:
+	print "Your Dead Wrong"
+	print "The Answer: Hurricane Sandy. In 2012 Sandy hit and left most of Red Hook with no water, electricity, or heat for almost a month."
+		
