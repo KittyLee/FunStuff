944020ead99bd273d0383a5ad16c0aba334a18e5
KittyLee <katherine@codeforprogress.org>
2014-05-19 16:53:27 -0400

    a game for my instructor

---
+++ Aliya_Game.py
@@ -0,0 +1,173 @@
+# So Is It Your Birthday?
+
+
+print "Welcome to: Your Game!!"
+
+#Section 1. First question
+print "1. Is today your Birthday?"
+print "\n(a) Yes  (b) No "
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "WELL HAPPY BIRTHDAY!!!!!"
+print "\n Now to the good stuff!!"
+print "------------------------------------------------------------"	
+
+
+#Section 2. Second question
+print "2. Would you like to see what Terri has to say?"
+print "\n(a) Yes  (b) No"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Hey I want you to tell Aliya that I said that (she is a bad ass) and I mean that in a good way. Thanks Terri"
+print "-------------------------------------------------------------"
+
+
+	#Section 3. Third question
+print "3. Would you like to see what Bobby Joe 3.0 has to say?"
+print "\n(a) Yes  (b) NO"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Aliyah, I think you're a total badass! I'm not sure how so much awesomeness can be contained in one little body...you defy physics?! You're wicked smaaart (said in my best Ben Affleck from A Good Will Hunting voice), super committed to social justice, and have already made a significant impact in my life. Thank you for your dedication, your love, and your patience! I feel like you give your all to us everyday, and it doesn't go unnoticed. Anyways, all of this is to say, Happy Birthday Aliyah! I'm excited I get to celebrate you on your special day. "
+print "------------------------------------------------------------"
+
+
+#Section 4. Forth question
+print "4. Would you like to see what Angie has to say?"
+print "\n(a) Yes  (b) NO"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Hey! Happy Birthday! It's been really awesome getting to know you for the past four weeks!You've taught me so much! So many tech skills and life skills in so little time it's unbelievable. Youve said things that I will carry with me for the rest of my life, and I cant thank you enough. You make this techie life look hella dope. The stuff you're doing with us is historic and I can only expect that we rise to the occasion and use these skills to give as of ourselves to the movement as you've given to us. Thanks for being my Harriet Techman on the Underground CodeRoad."
+print "------------------------------------------------------------"
+
+
+#Section 5. Fifth question
+print "5. Would you like to see what Dago has to say?"
+print "\n(a) Yes  (b) NO"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Thx for your awesome-ness I am inspired by all that you do!"
+print "------------------------------------------------------------"
+
+
+#Section 6. Sixth question
+print "6. Would you like to see what Pam has to say?"
+print "\n(a) Yes  (b) NO"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Python Queen.......I want to be like you when I get to Heaven!"
+print "------------------------------------------------------------"
+
+
+#Section 7. Seventh question
+print "7. Would you like to see what Kathy has to say?"
+print "\n(a) Yes  (b) NO"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "I think that your amazing in so many ways. Your such a bad ass but also very passionate about all the things you do which is awesome. Hope your birthday is full of enjoyment. P.S. I also like so many want to be like you when I grow up!!!! Best Wishes-Kathy"
+print "------------------------------------------------------------"
+
+
+#Section 8. Eighth question
+print "8. Would you like to see what Aurea has to say?"
+print "\n(a) Yes  (b) NO"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Allllllliiiiyyyaaaa!!!!!!!!!!!!! Feliz Cumpleanos!! Just wanted to let you know *drum roll* ............You are bomb.com!! Seriously your such a fierce, strong & beautiful mujer!! It has been an honer learning from you about coding and life in general. I truly appreciate all the positive energy, the great laughs and fun conversations we've shared. Love,Aurea"
+print "------------------------------------------------------------"
+
+
+#Section 9. Ninth question
+print "9. Would you like to see what Casidy has to say?"
+print "\n(a) Yes  (b) NO"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "I love how everyday you never seem to be bothered.Your constant, helpful, and most of all transparent. You teach one lesson in so many ways so that everyone is on the same page at the end of the day. most teachers teach just so say that they have a respectable profession... you stand out and im glad to be a student in the class! Thank you!  "
+print "------------------------------------------------------------"
+
+
+#Section 10. Tenth question
+print "10. Would you like to see what Mariella has to say?"
+print "\n(a) Yes  (b) NO"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Aliya has made me hate weekends because weekends mean no coding class which means not learning badassery coding kun fu skills that will allow me to learn to change the world. I want to be like Aliya when I grow up. no... seriously."
+print "------------------------------------------------------------"
+
+
+#Section 11. Eleven question
+print "11. Would you like to see what Jason has to say?"
+print "\n(a) Yes  (b) NO"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Yo Aliya, thanks for being a great teacher and a dope organizer/coder/super shero. You are changing lives - ours and the lives of the people that we'll touch through technology. Keep building bridges and breaking down walls. CFP14 got your back! - Jason "
+print "------------------------------------------------------------"
+
+
+#Section 12. Twelve question 
+print "12. Would you like to see what Elaine has to say?"
+print "\n(a) Yes  (b) No"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "HAPPY BIRTHDAY ALIYA! Its week 4 and I have written user stories, wire framed, drafted a database schema, ran a number of python scripts, made a really ugly website with html and CSS, and created a SQL database. All because of you and CFP. You are changing our lives, seriously. Also, thanks for your sudden raise of volume mid sentence while you're teaching. It keeps me on my toes."
+print "-------------------------------------------------------------"
+
+
+#Section 13. Fourteen question
+print "13. Would you like to see what Selina has to say?"
+print "\n(a)Yes  (b) No"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "You are the best teacher I have ever had--for realz.  Every morning I look forward to class.  At the end of the day,  I come home so happy.  I owe alot of that to your teaching style.  Thank you so much!"
+print "-------------------------------------------------------------"
+
+
+#Section 13. Thirten question
+print "13. Would you like to see what Stephen has to say?"
+print "\n(a) Yes  (b) No"
+
+
+answer = raw_input("Your answer: ")
+
+if answer=="a":
+	print "Happy Birthday! I hope your birthday is as awesome as the energy and passion you bring to the Code for Progress everyday. To many, many, many more!"
+print "-------------------------------------------------------------"
+
